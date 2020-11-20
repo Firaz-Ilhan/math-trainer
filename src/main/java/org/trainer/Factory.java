403d4a130c7e01175cd8acdbd050ddfca4b35caf
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2020-11-20 08:48:12 +0100

    Factory: add getRandomType()
    * Main: infinite loop for infinite fun

--- src/main/java/org/trainer/Factory.java
+++ src/main/java/org/trainer/Factory.java
@@ -1,6 +1,27 @@
 package org.trainer;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Factory {
+
+    private Random rand = new Random();
+
+    public String getRandomType(Boolean addition, boolean multiplication) {
+
+        ArrayList<String> typeList = new ArrayList<>(2);
+
+        if (addition) {
+            typeList.add("addition");
+        }
+
+        if (multiplication) {
+            typeList.add("multiplication");
+        }
+
+        String randomElement = typeList.get(rand.nextInt(typeList.size()));
+        return randomElement;
+    }
 
     public Arithmetic getArithmetic(String arithmeticType, String difficulty) {
 
@@ -16,4 +37,3 @@ public class Factory {
         return null;
     }
 }
-
--- src/main/java/org/trainer/Main.java
+++ src/main/java/org/trainer/Main.java
@@ -8,15 +8,17 @@ public class Main {
 
         Factory f1 = new Factory();
 
-        Arithmetic add = f1.getArithmetic("addition", "medium");
-
         try (final Scanner scanner = new Scanner(System.in)) {
 
             int userInput;
 
-            for (int i = 0; i < 10; i++) {
-                int[] result = add.getTask();
-                System.out.println("calculate: " + result[0] + add.getSign() + result[1]);
+            do {
+                String randomType = f1.getRandomType(true, true);
+
+                Arithmetic task = f1.getArithmetic(randomType, "easy");
+
+                int[] result = task.getTask();
+                System.out.println("calculate: " + result[0] + task.getSign() + result[1]);
 
                 userInput = scanner.nextInt();
                 if (userInput == result[2]) {
@@ -24,7 +26,7 @@ public class Main {
                 } else {
                     System.out.println(userInput + " isn't correct. " + result[2] + " is correct");
                 }
-            }
+            } while (true);
         }
     }
 }
