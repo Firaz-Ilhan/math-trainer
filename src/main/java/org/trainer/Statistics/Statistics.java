381b6057a0baed84da7ca9e3cd049688a793b0ce
MaxDol <maximilian.dolbaum@gmail.com>
2020-12-03 19:01:46 +0100

    prototype theoretically working one Exception to deal with implementation into checkSolution needed

--- src/main/java/org/trainer/Statistics/Statistics.java
+++ src/main/java/org/trainer/Statistics/Statistics.java
@@ -4,23 +4,30 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-
-
-
+ //currently stats for add sub mul div exp root ordOfOp
 public class Statistics {
     private int[][] currentCollection;
+     int[][] tempCollection;
     private final String fileName = "stats.txt";
 
     public static void main(String[] args) throws IOException { //test purposes only
         Statistics test = new Statistics();
-        System.out.println(Paths.get("stats.txt"));
         test.collector("AddItIon",true);
         test.statCombiner();
     }
 
-    private Statistics() {
-        this.currentCollection = new int[][] {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}};
+    private Statistics() throws IOException {
+        currentCollection = new int[][] {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}};
+        tempCollection = new int[][] {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}};
+        if(!Files.exists(Paths.get(fileName))){
+            createInitialStatFile();
+        }
+    }
 
+    private void createInitialStatFile() throws IOException {
+        FileWriter initial = new FileWriter(fileName);
+        initial.write(" 0 0 0 0 0 0 0 0 0 0 0 0 ");
+        initial.close();
     }
 
     private void collector(String taskType, boolean result) {
@@ -30,45 +37,60 @@ public class Statistics {
                 if (result) {
                     currentCollection[0][1] =+ 1;
                 }
+                break;
             case "subtraction":
                 currentCollection[1][0] =+ 1;
                 if (result) {
                     currentCollection[1][1] =+ 1;
                 }
+                break;
             case "multiplication":
                 currentCollection[2][0] =+ 1;
                 if (result) {
                     currentCollection[2][1] =+ 1;
                 }
+                break;
             case "division":
                 currentCollection[3][0] =+ 1;
                 if (result) {
                     currentCollection[3][1] =+ 1;
                 }
+                break;
             case "exponent":
                 currentCollection[4][0] =+ 1;
                 if (result) {
                     currentCollection[4][1] =+ 1;
                 }
+                break;
             case "root":
                 currentCollection[5][0] =+ 1;
                 if (result) {
                     currentCollection[5][1] =+ 1;
                 }
+                break;
+            case "orderofoperation":
+                currentCollection[6][0] =+ 1;
+                if (result) {
+                    currentCollection[6][1] =+ 1;
+                }
+                break;
         }
     }
 
     protected void statCombiner() throws IOException {
         String fileContent = Files.readString(Paths.get(fileName));
         String[] splitFileContent = fileContent.split(" ");
-        int counter = -1;
+        int counter = 0;
         StringBuilder toWrite = new StringBuilder();
+        toWrite.append(' ');
+
         for (int i = 0; i < currentCollection.length; i++) {
-            toWrite.append(currentCollection[i][0] =+ Integer.parseInt(splitFileContent[counter++]));
+            toWrite.append(tempCollection[i][0] = currentCollection[i][0] + Integer.parseInt(splitFileContent[counter++]));
             toWrite.append(' ');
-            toWrite.append(currentCollection[i][1] =+ Integer.parseInt(splitFileContent[counter++]));
+            toWrite.append(tempCollection[i][1] = currentCollection[i][1] + Integer.parseInt(splitFileContent[counter++]));
             toWrite.append(' ');
             }
+        Files.deleteIfExists(Paths.get(fileName));
         statSaver(toWrite.toString());
 
     }
@@ -77,10 +99,12 @@ public class Statistics {
         FileWriter writer = new FileWriter(fileName,false);
         writer.write(statString);
         writer.close();
+        tempCollection = new int[][] {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}};
     }
 
     protected void statReset() throws IOException {
         Files.deleteIfExists(Paths.get(fileName));
+        createInitialStatFile();
     }
 
 }
