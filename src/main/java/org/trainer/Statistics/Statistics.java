906f86d49c1452c57c5cd16f7ba4bf99bd38728a
MaxDol <maximilian.dolbaum@gmail.com>
2020-12-03 13:50:58 +0100

    in Statistics: added constructor, collector, statCombiner and statSaver

--- src/main/java/org/trainer/Statistics/Statistics.java
+++ src/main/java/org/trainer/Statistics/Statistics.java
@@ -1,4 +1,31 @@
 package org.trainer.Statistics;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.ArrayList;
+
 public class Statistics {
+    ArrayList<String> currentTaskCollection;
+    ArrayList<Boolean> currentResultCollection;
+
+    private Statistics() {
+        this.currentTaskCollection = new ArrayList<>();
+        this.currentResultCollection = new ArrayList<>();
+    }
+
+    private void collector(String taskType, boolean result) {
+        currentTaskCollection.add(taskType);
+        currentResultCollection.add(result);
+    }
+
+    protected void statCombiner(){
+        //reader
+        //add current Collections
+        //call statSaver
+        statSaver();
+    }
+
+    protected void statSaver(){
+
+    }
 }
