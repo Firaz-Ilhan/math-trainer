d0f9b2eb714a0730f631957d36f36d10e091b984
J3ns6 <jens.b.schlegel@gmail.com>
2020-12-04 21:21:20 +0100

    junit test for addition check solution in difficult range

--- src/test/java/org/trainer/AppTest.java
+++ src/test/java/org/trainer/AppTest.java
@@ -3,6 +3,7 @@ package org.trainer;
 import org.junit.Assert;
 import org.junit.Test;
 import org.trainer.Exercise.Addition;
+import org.trainer.Exercise.Arithmetic;
 
 /**
  * Unit test for simple App.
@@ -11,8 +12,19 @@ public class AppTest {
     /**
      * Dummy test method
      */
-    @Test
-    public void testApp() {
-        Assert.assertTrue( true );
+
+    @Test public void test_addition_beginner() {
+        Arithmetic add = new Addition("beginner");
+        Assert.assertTrue( add.getSolution(add.getTask()) >= 4 && add.getSolution(add.getTask()) <= 80) ;
+    }
+
+    @Test public void test_addition_medium() {
+        Arithmetic add = new Addition("medium");
+        Assert.assertTrue( add.getSolution(add.getTask()) >= 42 && add.getSolution(add.getTask()) <= 82) ;
+    }
+
+    @Test public void test_addition_hard() {
+        Arithmetic add = new Addition("hard");
+        Assert.assertTrue( add.getSolution(add.getTask()) >= 62 && add.getSolution(add.getTask()) <= 102) ;
     }
 }
