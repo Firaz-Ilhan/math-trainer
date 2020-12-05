d78fe5e08c48497cf00e4df96dd7f154c1481427
J3ns6 <jens.b.schlegel@gmail.com>
2020-12-05 11:54:35 +0100

    updated range test addition

--- src/test/java/org/trainer/AppTest.java
+++ src/test/java/org/trainer/AppTest.java
@@ -9,22 +9,23 @@ import org.trainer.Exercise.Arithmetic;
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Dummy test method
-     */
 
+    /* -- Addition -- */
     @Test public void test_addition_beginner() {
         Arithmetic add = new Addition("beginner");
-        Assert.assertTrue( add.getSolution(add.getTask()) >= 4 && add.getSolution(add.getTask()) <= 80) ;
+        Assert.assertTrue( add.getSolution(add.getTask()) >= -34 && add.getSolution(add.getTask()) <= 80) ;
     }
 
     @Test public void test_addition_medium() {
         Arithmetic add = new Addition("medium");
-        Assert.assertTrue( add.getSolution(add.getTask()) >= 42 && add.getSolution(add.getTask()) <= 82) ;
+        Assert.assertTrue( add.getSolution(add.getTask()) >= 22 && add.getSolution(add.getTask()) <= 82) ;
     }
 
     @Test public void test_addition_hard() {
         Arithmetic add = new Addition("hard");
-        Assert.assertTrue( add.getSolution(add.getTask()) >= 62 && add.getSolution(add.getTask()) <= 102) ;
+        Assert.assertTrue( add.getSolution(add.getTask()) >= 42 && add.getSolution(add.getTask()) <= 102) ;
     }
+
+    /* -- Multiplication -- */
+
 }
