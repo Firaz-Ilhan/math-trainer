0dbdc02c7db29f3c20cddb5c722527d1cef955c8
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2020-12-17 17:36:08 +0100

    setting.fxml: change font size and button text
    Use "Main Menu" instead of "Go Back"

--- src/main/java/org/trainer/GUI/SettingController.java
+++ src/main/java/org/trainer/GUI/SettingController.java
@@ -2,12 +2,25 @@ package org.trainer.GUI;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 
 public class SettingController extends Controller {
 
     @FXML
     private GridPane root;
+
+    @FXML
+    private CheckBox additionCheckbox, subtractionCheckbox, multiplicationCheckbox, divisionCheckbox,
+            orderOfOperationCheckbox, exponentCheckbox, rootCheckbox;
+
+    @FXML
+    private RadioButton beginnerRadioButton, mediumRadioButton, hardRadioButton;
+
+    @FXML
+    private ToggleGroup difficulty;
 
     public void openMenu(ActionEvent actionEvent) {
         changeScene(MAIN_MENU_FXML, root);
