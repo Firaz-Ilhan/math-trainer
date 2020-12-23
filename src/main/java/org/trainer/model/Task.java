e5076ad2abd4378ed468f3398255539e02a0b0f3
MaxDol <maximilian.dolbaum@gmail.com>
2020-12-23 15:30:50 +0100

    Javadoc Task

--- src/main/java/org/trainer/model/Task.java
+++ src/main/java/org/trainer/model/Task.java
@@ -6,6 +6,12 @@ public class Task {
 
     final Random rand = new Random();
 
+    /**
+     * Checks if the user input matches the calculated solution.
+     * @param task int array with task numbers and solution.
+     * @param input the users input.
+     * @return boolean if solved successfully.
+     */
     public boolean checkSolution(int[] task, int input) {
         if (task == null || task.length == 0) {
             return false;
@@ -14,6 +20,11 @@ public class Task {
         }
     }
 
+    /**
+     * Returns the solution provided in the last array slot.
+     * @param task int array with task numbers and solution.
+     * @return solution int.
+     */
     public int getSolution(int[] task) {
         return task[task.length - 1];
     }
