ad23ba893f5c4b9a00596591466cf71516db17e7
MaxDol <maximilian.dolbaum@gmail.com>
2020-12-23 15:31:49 +0100

    Javadoc Division

--- src/main/java/org/trainer/model/Division.java
+++ src/main/java/org/trainer/model/Division.java
@@ -7,6 +7,10 @@ public class Division extends Task implements Arithmetic {
     final Logger log = LogManager.getLogger(Division.class);
     private String difficulty;
 
+    /**
+     * Constructor.
+     * @param difficulty setting the difficulty for {@link #getTask()}.
+     */
     public Division(String difficulty) {
         this.difficulty = difficulty.toLowerCase();
     }
