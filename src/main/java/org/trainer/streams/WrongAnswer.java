3df48e211de7324f9d54c0dd9861b3c1010ba321
J3ns6 <jens.b.schlegel@gmail.com>
2021-01-21 19:59:38 +0100

    removed unused import statment

--- src/main/java/org/trainer/streams/WrongAnswer.java
+++ src/main/java/org/trainer/streams/WrongAnswer.java
@@ -3,7 +3,6 @@ package org.trainer.streams;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
