8783fd9c7b29c71788e76cfc6a5f538392b264c8
MaxDol <maximilian.dolbaum@gmail.com>
2021-01-29 00:28:30 +0100

    JavaDoc in Launcher

--- src/main/java/org/trainer/Launcher.java
+++ src/main/java/org/trainer/Launcher.java
@@ -4,6 +4,11 @@ import org.trainer.gui.FxmlGuiDriver;
 
 public class Launcher {
 
+    /**
+     * Main method calling FxmlGuiDriver.main to Launch the game
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         FxmlGuiDriver.main(args);
     }
