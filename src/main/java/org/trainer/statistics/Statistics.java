07f996ee1838353dff51240e88f9dd3debff64a9
MaxDol <maximilian.dolbaum@gmail.com>
2021-01-29 00:21:06 +0100

    JavaDoc and Commentary for Statistics

--- src/main/java/org/trainer/statistics/Statistics.java
+++ src/main/java/org/trainer/statistics/Statistics.java
@@ -14,7 +14,7 @@ import java.util.Objects;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
-//currently stats for add sub mul div exp root ordOfOp
+//currently stats for add sub mul div exp root orderOfOperation
 public class Statistics {
     private final String[] operations = {"Addition", "Subtraction", "Multiplication", "Division", "Exponent", "Root", "Order of Operation"};
     private static final Logger log = LogManager.getLogger(Statistics.class);
@@ -26,7 +26,7 @@ public class Statistics {
     /**
      * Constructor with creation of the necessary initial forms for file and array.
      */
-    public Statistics() throws IOException {
+    public Statistics() throws IOException { //Constructor sets everything up needed for Statistics
         createInitialStatArrays();
         if (!Files.exists(Paths.get(fileName))) {
             createInitialStatFile();
@@ -37,8 +37,8 @@ public class Statistics {
      * This method creates the initial Arrays with operations.length-1
      */
     private void createInitialStatArrays() {
-        currentCollection = new int[operations.length][2];
-        tempCollection = new int[operations.length][2];
+        currentCollection = new int[operations.length][2];  //setting the total task and successful completed tasks
+        tempCollection = new int[operations.length][2];     //to 0 for all TaskTypes in operations
         for (int i = 0; i < operations.length; i++) {
             currentCollection[i][0] = 0;
             currentCollection[i][1] = 0;
@@ -55,18 +55,18 @@ public class Statistics {
         final StringBuilder initialString = new StringBuilder();
         Stream<String> operationsStream = Stream.of(operations);
 
-        operationsStream
+        operationsStream //Stream to check how many Actual taskTypes there are in "operations" Array
                 .filter(Objects::nonNull)
                 .filter(Predicate.not(String::isEmpty))
-                .forEach(i -> initialString.append("0 0 "));
+                .forEach(i -> initialString.append("0 0 ")); //writing the String
 
         initial.write(initialString.toString());
-        initial.close();
+        initial.close(); //closing the FileWriter
         log.info("Creating the default stat file");
     }
 
     /**
-     * The collector collects information on the solved state after each task.
+     * The collector collects information on the solved state after each task is answered.
      *
      * @param taskType String with information of the Type.
      * @param result   boolean for solved successfully.
@@ -117,9 +117,9 @@ public class Statistics {
                 break;
         }
         if (result) {
-            log.info("Adding a correct answer to the statistics\n");
+            log.info("Adding a correct answer for " + taskType + ".\n");
         } else {
-            log.info("Adding an incorrect answer to the statistics\n");
+            log.info("Adding an incorrect answer for " + taskType + ".\n");
         }
     }
 
@@ -158,13 +158,13 @@ public class Statistics {
             String fileContent = reader.readLine();
             reader.close();
             log.info("Current stats: {}", fileContent);
-            final String[] splitFileContent = fileContent.split(" ");
+            final String[] splitFileContent = fileContent.split(" "); //splitting stats.txt in the single numbers
             int counter = 0;
             final StringBuilder toWrite = new StringBuilder();
-            for (int i = 0; i < currentCollection.length; i++) {
-                toWrite.append(tempCollection[i][0] = currentCollection[i][0] + Integer.parseInt(splitFileContent[counter++]));
+            for (int i = 0; i < currentCollection.length; i++) { //building the new String
+                toWrite.append(tempCollection[i][0] = currentCollection[i][0] + Integer.parseInt(splitFileContent[counter++])); //getting the total task information
                 toWrite.append(' ');
-                toWrite.append(tempCollection[i][1] = currentCollection[i][1] + Integer.parseInt(splitFileContent[counter++]));
+                toWrite.append(tempCollection[i][1] = currentCollection[i][1] + Integer.parseInt(splitFileContent[counter++])); //getting the successful finished task information
                 if (i != currentCollection.length - 1) {
                     toWrite.append(' ');
                 }
@@ -184,10 +184,10 @@ public class Statistics {
     public void statSaver() throws IOException {
         statCombiner();
         FileWriter writer = new FileWriter(fileName, false);
-        writer.write(tempStats);
+        writer.write(tempStats); //saving the String set in statCombiner()
         writer.close();
         log.info("Saving the current stats to lifetime stats");
-        tempCollection = new int[][]{{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}};
+        tempCollection = new int[][]{{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}; //resetting the Arrays
         currentCollection = new int[][]{{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}};
     }
 
@@ -196,7 +196,7 @@ public class Statistics {
      */
     public void statReset() throws IOException {
         log.info("Resetting the stats");
-        createInitialStatFile();
+        createInitialStatFile(); //forcing the function to be called to overwrite the current stats.txt
     }
 
     /**
@@ -208,9 +208,9 @@ public class Statistics {
      */
     public int percentStat(int[][] arr, int pos) {
         if (arr[pos][0] == 0) {
-            return 100;
+            return 100; //base stat is 100% out of 0 calculations solved correctly
         } else {
-            float percent = (arr[pos][1] / (float) arr[pos][0]) * 100.0F;
+            float percent = (arr[pos][1] / (float) arr[pos][0]) * 100.0F; //float allows work with decimal stats
             return Math.round(percent);
         }
     }
@@ -223,13 +223,13 @@ public class Statistics {
      */
     public int[] getPercentStats() {
         int[][] array;
-        int[] stats = new int[7];
-        statCombiner();
+        int[] percentStats = new int[7];
+        statCombiner(); //making sure the stats are up toDate
         array = tempCollection;
         for (int position = 0; position < currentCollection.length; position++) {
-            stats[position] = percentStat(array, position);
+            percentStats[position] = percentStat(array, position);
         }
-        return stats.clone();
+        return percentStats.clone();
     }
 
     /**
@@ -238,7 +238,7 @@ public class Statistics {
      * @param lifetime decides whether it is all or current stats.
      * @return returns the String.
      */
-    public String getStats(boolean lifetime) { //currently only in Main, if not needed delete method
+    public String getStats(boolean lifetime) {
         int[][] array;
         String whatStats;
         if (lifetime) {
@@ -252,7 +252,7 @@ public class Statistics {
 
         StringBuilder displayStats = new StringBuilder();
         displayStats.append(whatStats).append("\n");
-        for (int i = 0; i < operations.length; i++) {
+        for (int i = 0; i < operations.length; i++) { //a for loop to write out stats for every TaskType in operations
             displayStats.append(operations[i]).append(": ").append(percentStat(array, i)).append("% out of ").append(array[i][0]).append(" tasks");
             if (i != operations.length - 1) {
                 displayStats.append("\n");
