b9bcf266c49a05161e745e98b4df227877c75cc6
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2021-01-30 00:26:34 +0100

    prevent Helper from being instantiated
    * add private empty constructor
    * make Helper final
    * add comments

--- src/main/java/org/trainer/model/Helper.java
+++ src/main/java/org/trainer/model/Helper.java
@@ -2,15 +2,22 @@ package org.trainer.model;
 
 import java.util.regex.Pattern;
 
-public class Helper {
+/**
+ * Only static access allowed
+ */
+public final class Helper {
 
     private static final Pattern pattern = Pattern.compile("-?[0-9]{0,10}");
+
+    //prevents class from being instantiated
+    private Helper() {
+    }
 
     /**
      * Checks if the users input is in the correct format pattern
      *
      * @param userInput the answer from the User
-     * @return boolean
+     * @return boolean true if user input matches pattern
      */
     public static boolean checkInputPattern(String userInput) {
         if (userInput.isEmpty()) {
